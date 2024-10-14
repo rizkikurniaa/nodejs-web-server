4a4af0f76f1e4695701017941bb90e9218b0bafd
rizki <rizkikurniawan1797@gmail.com>
2024-10-14 09:28:59 +0700

    response status

--- server.js
+++ server.js
@@ -2,28 +2,24 @@ const http = require("http");
 
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
-  response.statusCode = 200;
 
   const { method, url } = request;
 
   if (url === "/") {
-    // TODO 2: logika respons bila url bernilai '/'
     if (method === "GET") {
-      // response bila client menggunakan GET
+      response.statusCode = 200;
       response.end("<h1>Ini adalah homepage</h1>");
     } else {
-      // response bila client tidak menggunakan GET
+      response.statusCode = 400;
       response.end(
         `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
       );
     }
   } else if (url === "/about") {
-    // TODO 3: logika respons bila url bernilai '/about'
     if (method === "GET") {
-      // respons bila client menggunakan GET
+      response.statusCode = 200;
       response.end("<h1>Halo! Ini adalah halaman about</h1>");
     } else if (method === "POST") {
-      // respons bila client menggunakan POST
       let body = [];
 
       request.on("data", (chunk) => {
@@ -33,16 +29,17 @@ const requestListener = (request, response) => {
       request.on("end", () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
+        response.statusCode = 200;
         response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
       });
     } else {
-      // respons bila client tidak menggunakan GET ataupun POST
+      response.statusCode = 400;
       response.end(
         `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
       );
     }
   } else {
-    // TODO 1: logika respons bila url bukan '/' atau '/about'
+    response.statusCode = 404;
     response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
