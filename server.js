f296af1c74e47c045d6842873735543fc150af4d
rizki <rizkikurniawan1797@gmail.com>
2024-10-14 09:16:11 +0700

    routing

--- server.js
+++ server.js
@@ -4,24 +4,46 @@ const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
   response.statusCode = 200;
 
-  const { method } = request;
+  const { method, url } = request;
 
-  if (method === "GET") {
-    response.end("<h1>Hello!</h1>");
-  }
+  if (url === "/") {
+    // TODO 2: logika respons bila url bernilai '/'
+    if (method === "GET") {
+      // response bila client menggunakan GET
+      response.end("<h1>Ini adalah homepage</h1>");
+    } else {
+      // response bila client tidak menggunakan GET
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else if (url === "/about") {
+    // TODO 3: logika respons bila url bernilai '/about'
+    if (method === "GET") {
+      // respons bila client menggunakan GET
+      response.end("<h1>Halo! Ini adalah halaman about</h1>");
+    } else if (method === "POST") {
+      // respons bila client menggunakan POST
+      let body = [];
 
-  if (method === "POST") {
-    let body = [];
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
 
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}!</h1>`);
-    });
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } else {
+      // respons bila client tidak menggunakan GET ataupun POST
+      response.end(
+        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
+      );
+    }
+  } else {
+    // TODO 1: logika respons bila url bukan '/' atau '/about'
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
 
